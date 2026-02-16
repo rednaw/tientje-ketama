1b92c899c27b8be43af921513f8379fdd615a3de
Wander Grevink <wander.grevink@gmail.com>
2026-02-16 12:37:24 +0000

    port mapping

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
+  server: {
+    port: parseInt(process.env.PORT || '5173', 10),
+  },
+  preview: {
+    port: parseInt(process.env.PORT || '4173', 10),
+  },
 });
