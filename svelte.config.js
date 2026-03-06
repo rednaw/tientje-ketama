bc9df7f73d65a24e25ad19e67941937e15e8a21b
Wander Grevink <wander.grevink@gmail.com>
2026-03-06 14:17:05 +0000

    csp

--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,17 @@ const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({ out: 'build' }),
+    csp: {
+      mode: 'nonce',
+      directives: {
+        'default-src': ['self'],
+        'script-src': ['self', 'https://scripts.simpleanalyticscdn.com'],
+        'style-src': ['self', 'unsafe-inline'], // Svelte transitions use inline styles
+        'img-src': ['self', 'data:'],
+        'connect-src': ['self', 'https://api.simpleanalytics.com'],
+        'media-src': ['self'],
+      },
+    },
   },
 };
 
